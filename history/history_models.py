b782f94feff3a020956af87b135ce24cfebeff01
Tran Son <transon@MacBook-Pro-cua-Tran.local>
2025-05-14 12:51:24 +0700

    fix history when garden_item get deleted

--- history/history_models.py
+++ history/history_models.py
@@ -15,5 +15,5 @@ class History(BaseModel):
     description = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_caring_history = models.BooleanField(default=False)
-    garden_item = models.ForeignKey(Garden, on_delete=models.CASCADE, null=True, blank=True)
+    garden_item = models.ForeignKey(Garden, on_delete=models.SET_NULL, null=True, blank=True)
     caring_type = models.CharField(max_length=50, null=True, blank=True, choices=CareType.choices)
