4735bab89e78a11f1c68245f8ed756ceca2bc031
TranSon <tson113355@gmail.com>
2025-05-23 10:20:28 +0700

    a

--- delivery/serializers.py
+++ delivery/serializers.py
@@ -3,7 +3,7 @@ from delivery.models import Delivery
 
 
 class DeliveryItemSerializer(serializers.Serializer):
-    item_id = serializers.IntergerField()
+    item_id = serializers.IntegerField()
     name = serializers.CharField()
     quantity = serializers.IntegerField()
     price = serializers.IntegerField()
